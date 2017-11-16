f99e98e7d880e555a0cebb6f70ebbd1ec188d355
Casey Copeland <casey@copeland.tech>
2017-11-15 22:07:58 -0600

    Added RabbitMQ Client side code.

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -1,12 +1,48 @@
-﻿using System;
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
 
 namespace Client
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
+            var connectionFactory = new ConnectionFactory() { HostName = "10.5.5.224" };
+
+            using (var connection = connectionFactory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.ExchangeDeclare(exchange: "alaska", type: "direct");
+
+                    var queueName = channel.QueueDeclare().QueueName;
+
+                    channel.QueueBind(queue: queueName,
+                                        exchange: "alaska",
+                                        routingKey: "");
+
+                    var consumer = new EventingBasicConsumer(channel);
+
+                    consumer.Received += (sender, eventArgs) =>
+                    {
+                        var messageBytes = eventArgs.Body;
+                        var message = Encoding.UTF8.GetString(messageBytes);
+
+                        Console.WriteLine($"Received message: {message}");
+
+                        channel.BasicAck(deliveryTag: eventArgs.DeliveryTag, multiple: false);
+                    };
+
+                    channel.BasicConsume(queue: queueName,
+                                         autoAck: false,
+                                         consumer: consumer);
+
+                    Console.WriteLine("Press Ctrl+C to exit.");
+                    Console.ReadLine();
+                }
+            }
         }
     }
 }
