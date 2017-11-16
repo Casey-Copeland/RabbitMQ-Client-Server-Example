ff7ec4315a28572724c55b5d8743b719b05a0860
Casey Copeland <casey@copeland.tech>
2017-11-15 22:11:27 -0600

    Added Server code for RabbitMQ connection.

--- src/Server/Program.cs
+++ src/Server/Program.cs
@@ -1,12 +1,35 @@
-﻿using System;
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
 
 namespace Server
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
+            var factory = new ConnectionFactory() { HostName = "10.5.5.224" };
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.ExchangeDeclare(exchange: "alaska", type: "direct");
+
+                    string message = "Hello from Texas!";
+                    var messageBytes = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "alaska",
+                                         routingKey: "",
+                                         basicProperties: null,
+                                         body: messageBytes);
+
+                    Console.WriteLine($"Sent message: {message}");
+                }
+            }
+
+            Console.WriteLine("Press Ctrl+C to exit.");
+            Console.ReadLine();
         }
     }
 }
